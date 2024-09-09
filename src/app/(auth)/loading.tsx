73050618dab7235cf75ca7f3dbe10173d2432616
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2024-09-09 21:21:56 +0700

    add page transition, loading

--- src/app/(auth)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import Loader from "@/app/_components/common/loader/loader";
-
-export interface ILoadingProps {}
-
-export default function LoadingPage(props: ILoadingProps) {
-  return <Loader />;
-}
--- src/app/(products)/products/loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import Loader from "@/app/_components/common/loader/loader";
-
-export interface ILoadingPageProps {}
-
-export default function LoadingPage(props: ILoadingPageProps) {
-  return <Loader />;
-}
